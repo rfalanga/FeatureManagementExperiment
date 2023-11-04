c77f4e9db7d5795a8c47768b72b3d1c9fae8611d
Rod Falanga <rfalanga@users.noreply.github.com>
2023-11-04 09:09:47 -0600

    initial commit

---
+++ FeatureManagementExperiment/FeatureManagementExperiment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeatureManagementExperiment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FeatureManagementExperiment
 Experimenting to learn ways of using Microsoft's FeatureManagement-Dotnet
+
+## .NET version
+This project was started as a .NET 7 app
