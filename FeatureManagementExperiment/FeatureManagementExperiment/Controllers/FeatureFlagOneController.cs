147315b48eea9deb56582f58297446ff8de13bb8
Rod Falanga <rfalanga@users.noreply.github.com>
2023-11-12 17:38:29 -0700

    added the controller

---
+++ FeatureManagementExperiment/FeatureManagementExperiment/Controllers/FeatureFlagOneController.cs
@@ -0,0 +1,16 @@
+﻿using FeatureManagementExperiment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+using System.Diagnostics;
+
+namespace FeatureManagementExperiment.Controllers
+{
+    public class FeatureFlagOneController : Controller
+    {
+        private readonly ILogger<FeatureFlagOneController> _logger;
+
+        public FeatureFlagOneController(ILogger<FeatureFlagOneController> logger) => _logger = logger;
+
+        public IActionResult Index() => View();
+    }
+}
